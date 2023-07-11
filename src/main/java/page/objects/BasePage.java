92ff4a40724079830b098d84c9f47a0324944d4f
Konrad Sar <konradsar2015@gmail.com>
2023-07-11 11:00:45 +0200

    Simplify code with test data reader

--- src/main/java/page/objects/BasePage.java
+++ src/main/java/page/objects/BasePage.java
@@ -1,8 +1,7 @@
 package page.objects;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
-import test.data.TestDataReader;
+import test.data.TestData;
 import test.data.model.Address;
 import test.data.model.Credentials;
 import test.data.model.Url;
@@ -16,14 +15,14 @@ import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 public class BasePage {
-    protected static TestDataReader testDataReader;
+    protected static TestData testDataReader;
     private final static String TEST_DATA_LOCATION = "src/configs/Configuration.properties";
     protected WebDriver driver;
 
     public static void loadConfig() throws IOException {
         Properties properties = new Properties();
         properties.load(new InputStreamReader(new FileInputStream(TEST_DATA_LOCATION), StandardCharsets.UTF_8));
-        testDataReader = new TestDataReader(new Cookie(properties), new Cookie(properties), new User(properties),
+        testDataReader = new TestData(new Cookie(properties), new Cookie(properties), new User(properties),
                 new User(properties), new User(properties), new User(properties), new Address(properties),
                 new Address(properties), new Address(properties), new Url(properties), new Url(properties),
                 new Url(properties), new Url(properties), new Url(properties), new Url(properties), new Url(properties),
--- src/main/java/test/data/TestDataReader.java
+++ src/main/java/test/data/TestDataReader.java
@@ -1,131 +1,18 @@
 package test.data;
 
-import test.data.model.Address;
-import test.data.model.Credentials;
-import test.data.model.Url;
-import test.data.model.User;
-import utils.cookie.Cookie;
+import lombok.SneakyThrows;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 
 public class TestDataReader {
-    private Cookie cookie;
-    private Cookie attribute;
-    private User name;
-    private User surname;
-    private User phone;
-    private User email;
-    private Address street;
-    private Address postalCode;
-    private Address city;
-    private Url japanCandiesDescendingPrice;
-    private Url barsResults2ndSite;
-    private Url barsResults1stSite;
-    private Url japanCandies;
-    private Url tajlandCandies;
-    private Url salsaDipDetails;
-    private Url basket;
-    private Url mainPageSource;
-    private Credentials eMail;
-    private Credentials password;
-
-    public TestDataReader(Cookie cookieName, Cookie cookieAttribute, User name, User surname,
-                          User phone, User email, Address city, Address street, Address postalCode,
-                          Url japanCandiesResults4thSite, Url barsResults2ndSite, Url barsResults1stSite,
-                          Url japanCandies, Url tajlandCandies, Url salsaDipDetails, Url basketSite,
-                          Url baseURL, Credentials eMail, Credentials password) {
-        this.cookie = cookieName;
-        this.attribute = cookieAttribute;
-        this.name = name;
-        this.surname = surname;
-        this.phone = phone;
-        this.email = email;
-        this.city = city;
-        this.street = street;
-        this.postalCode = postalCode;
-        this.japanCandiesDescendingPrice = japanCandiesResults4thSite;
-        this.barsResults2ndSite = barsResults2ndSite;
-        this.barsResults1stSite = barsResults1stSite;
-        this.japanCandies = japanCandies;
-        this.tajlandCandies = tajlandCandies;
-        this.salsaDipDetails = salsaDipDetails;
-        this.basket = basketSite;
-        this.mainPageSource = baseURL;
-        this.eMail = eMail;
-        this.password = password;
-    }
-
-    public Cookie getAttribute() {
-        return attribute;
-    }
-
-    public Cookie getCookie() {
-        return cookie;
-    }
-
-    public User getName() {
-        return name;
-    }
-
-    public User getSurname() {
-        return surname;
-    }
-
-    public User getPhone() {
-        return phone;
-    }
-
-    public User getEmail() {
-        return email;
-    }
-
-    public Address getStreet() {
-        return street;
-    }
-
-    public Address getPostalCode() {
-        return postalCode;
-    }
-
-    public Address getCity() {
-        return city;
-    }
-
-    public Url getJapanCandiesDescendingPriceResults() {
-        return japanCandiesDescendingPrice;
-    }
-
-    public Url getBarsResults2ndSite() {
-        return barsResults2ndSite;
-    }
-
-    public Url getBarsResults1stSite() {
-        return barsResults1stSite;
-    }
-
-    public Url getJapanCandies() {
-        return japanCandies;
-    }
-
-    public Url getTajlandCandies() {
-        return tajlandCandies;
-    }
-
-    public Url getSalsaDipDetails() {
-        return salsaDipDetails;
-    }
-
-    public Url getBasket() {
-        return basket;
-    }
-
-    public Url getMainPageSource() {
-        return mainPageSource;
-    }
-
-    public Credentials geteMail() {
-        return eMail;
-    }
-
-    public Credentials getPassword() {
-        return password;
+    private final static String TEST_DATA_LOCATION = "src/configs/Configuration.properties";
+    @SneakyThrows
+    public static TestData loadTestData(){
+        Properties properties = new Properties();
+        properties.load(new InputStreamReader(new FileInputStream(TEST_DATA_LOCATION), StandardCharsets.UTF_8));
+        return new TestData(properties);
     }
 }
