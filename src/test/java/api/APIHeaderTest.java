fe5257928b26a8522eca0d8f1c4419df841bda67
Konrad Sar <konradsar2015@gmail.com>
2023-07-14 13:52:48 +0200

    add allure, junit5 library, add pom.xml dependencies

--- src/test/java/api/APIHeaderTest.java
+++ src/test/java/api/APIHeaderTest.java
@@ -1,14 +1,10 @@
 package api;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import utils.config.Requests;
 
-public class APIHeaderTests extends BaseAPITest {
-
-    private static Logger logger = LogManager.getLogger(APIHeaderTests.class);
+public class APIHeaderTest extends BaseAPITest {
 
     @Test
     public void When_SupplyIconIsClicked_Expect_SupplyDetailPageIsDisplayed() {
